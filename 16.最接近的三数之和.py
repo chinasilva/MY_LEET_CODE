f969d67018a890143ff97e196cec0e389a95913b
chinasilva <www476369545@qq.com>
2019-05-20 22:32:40 +0800

    新增两道题目

--- 16.最接近的三数之和.py
+++ 16.最接近的三数之和.py
@@ -13,14 +13,39 @@
 #
 # 给定一个包括 n 个整数的数组 nums 和 一个目标值 target。找出 nums 中的三个整数，使得它们的和与 target
 # 最接近。返回这三个数的和。假定每组输入只存在唯一答案。
-# 
-# 例如，给定数组 nums = [-1，2，1，-4], 和 target = 1.
-# 
-# 与 target 最接近的三个数的和为 2. (-1 + 2 + 1 = 2).
-# 
-# 
 #
-class Solution:
-    def threeSumClosest(self, nums: List[int], target: int) -> int:
-        
+# 例如，给定数组 nums = [-1，2，1，-4], 和 target = 1.
+#
+# 与 target 最接近的三个数的和为 2. (-1 + 2 + 1 = 2).
+#
+#
+#
 
+
+class Solution:
+    def threeSumClosest(self, nums, target: int) -> int:
+        newNums = sorted(nums)
+        res = sum(newNums[:3])
+        for i in range(len(nums)-2):
+            # print(res)
+            l = i+1
+            r = len(nums)-1
+            #print(newNums)
+            while l < r:
+                sumNum = newNums[i]+newNums[l]+newNums[r]
+                if target == sumNum:
+                    return sumNum
+                elif abs(res-target) > abs(sumNum-target):
+                    res = sumNum
+                elif target > sumNum:
+                    l = l+1
+                elif target < sumNum:
+                    r = r-1
+        return res
+
+
+if __name__ == "__main__":
+    s = Solution()
+    # s.threeSum([7,-1,14,-12,-8,7,2,-15,8,8,-8,-14,-4,-5,7,9,11,-4,-15,-6,1,-14,4,3,10,-5,2,1,6,11,2,-2,-5,-7,-6,2,-15,11,-6,8,-4,2,1,-1,4,-6,-15,1,5,-15,10,14,9,-8,-6,4,-6,11,12,-15,7,-1,-9,9,-1,0,-4,-1,-12,-2,14,-9,7,0,-3,-4,1,-2,12,14,-10,0,5,14,-1,14,3,8,10,-8,8,-5,-2,6,-11,12,13,-7,-12,8,6,-13,14,-2,-5,-11,1,3,-6])
+    # s.threeSum([-1, 0, 1, 2, -1, -4])
+    print(s.threeSumClosest([-1, 2, 1, -4], 1))
---
+++ 20.有效的括号.py
@@ -0,0 +1,110 @@
+#
+# @lc app=leetcode.cn id=20 lang=python3
+#
+# [20] 有效的括号
+#
+# https://leetcode-cn.com/problems/valid-parentheses/description/
+#
+# algorithms
+# Easy (38.04%)
+# Total Accepted:    75.4K
+# Total Submissions: 198K
+# Testcase Example:  '"()"'
+#
+# 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+#
+# 有效字符串需满足：
+#
+#
+# 左括号必须用相同类型的右括号闭合。
+# 左括号必须以正确的顺序闭合。
+#
+#
+# 注意空字符串可被认为是有效字符串。
+#
+# 示例 1:
+#
+# 输入: "()"
+# 输出: true
+#
+#
+# 示例 2:
+#
+# 输入: "()[]{}"
+# 输出: true
+#
+#
+# 示例 3:
+#
+# 输入: "(]"
+# 输出: false
+#
+#
+# 示例 4:
+#
+# 输入: "([)]"
+# 输出: false
+#
+#
+# 示例 5:
+#
+# 输入: "{[]}"
+# 输出: true
+#
+#
+
+
+class Solution:
+    def __init__(self):
+        self.dic = {'[': -1, '{': -2, '(': -3, ']': 1, '}': 2, ')': 3}
+
+    def isValid(self, s: str) -> bool:
+        # s=set(s)
+        sLen = len(s)
+        # print(self.dic[s[0]])
+        if sLen % 2 != 0:  # 奇数个数则错误
+            return False
+        if self.fun1(s) or self.fun2(s):
+            return True
+        else:
+            return False
+
+    def fun1(self, s) -> bool:
+        '''
+        # 从中间向两边d移动
+        '''
+        sLen = len(s)
+        L = sLen // 2 - 1
+        R = sLen//2
+        while R < sLen:
+            # print(s[L])
+            if self.dic[s[L]]+self.dic[s[R]] != 0:
+                return False
+            L = L-1
+            R = R+1
+        return True
+
+    def fun2(self, s) -> bool:
+        '''
+        # 从左向右移动
+        '''
+        sLen = len(s)
+        for i in range(0, sLen, 2):
+            L = i
+            R = i+1
+            if self.dic[s[L]]+self.dic[s[R]] != 0:
+                return False
+        return True
+
+
+if __name__ == "__main__":
+    s = Solution()
+  #  str0 = "()"
+  #  str0 = "()[]{}"
+   # str0 = "(]"
+#    str0 = "([)]"
+   # str0 = "{[]}"
+    # str0 = "["
+    str0="(([]){})"
+
+    print(s.isValid(str0))
