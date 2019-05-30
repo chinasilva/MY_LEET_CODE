ab0db00288f6dd8c2880798772f6d4a72080f968
chinasilva <www476369545@qq.com>
2019-05-30 23:30:21 +0800

    螺旋矩阵

---
+++ 54.螺旋矩阵.py
@@ -0,0 +1,78 @@
+#
+# @lc app=leetcode.cn id=54 lang=python3
+#
+# [54] 螺旋矩阵
+#
+# https://leetcode-cn.com/problems/spiral-matrix/description/
+#
+# algorithms
+# Medium (35.54%)
+# Total Accepted:    14.6K
+# Total Submissions: 41.1K
+# Testcase Example:  '[[1,2,3],[4,5,6],[7,8,9]]'
+#
+# 给定一个包含 m x n 个元素的矩阵（m 行, n 列），请按照顺时针螺旋顺序，返回矩阵中的所有元素。
+# 
+# 示例 1:
+# 
+# 输入:
+# [
+# ⁠[ 1, 2, 3 ],
+# ⁠[ 4, 5, 6 ],
+# ⁠[ 7, 8, 9 ]
+# ]
+# 输出: [1,2,3,6,9,8,7,4,5]
+# 
+# 
+# 示例 2:
+# 
+# 输入:
+# [
+# ⁠ [1, 2, 3, 4],
+# ⁠ [5, 6, 7, 8],
+# ⁠ [9,10,11,12]
+# ]
+# 输出: [1,2,3,4,8,12,11,10,9,5,6,7]
+# 
+# 
+#
+class Solution:
+    def spiralOrder(self, matrix):
+ '''
+spiral_order([[1, 2, 3],
+                [4, 5, 6],
+                [7, 8, 9]])
+
+= [1, 2, 3] + spiral_order([[6, 9],
+                            [5, 8],
+                            [4, 7]])
+
+= [1, 2, 3] + [6, 9] + spiral_order([[8, 7],
+                                     [5, 4]])
+
+= [1, 2, 3] + [6, 9] + [8, 7] + spiral_order([[4],
+                                              [5]])
+
+= [1, 2, 3] + [6, 9] + [8, 7] + [4] + spiral_order([[5]])
+
+= [1, 2, 3] + [6, 9] + [8, 7] + [4] + [5] + spiral_order([])
+
+= [1, 2, 3] + [6, 9] + [8, 7] + [4] + [5] + []
+
+= [1, 2, 3, 6, 9, 8, 7, 4, 5]
+'''       
+        #每次变换前取第一行
+        #print(*[matrix.pop(0)])
+        
+        #对矩阵进行形状变换,倒序。并且递归调用进行取值
+        #print([*zip(*matrix)][::-1])
+        
+        return matrix and [*matrix.pop(0)] + self.spiralOrder([*zip(*matrix)][::-1])
+
+if __name__ == "__main__":
+    s = Solution()
+    lst= [[1, 2, 3, 4], [5, 6, 7, 8], [9,10,11,12]]
+    #lst= [-2,1,-3,4,-1,2,1,-5,4]
+    #  ,[1,2,3]
+    print(s.spiralOrder(lst))
+
